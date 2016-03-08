a1d73e550bfcf32a764ba1717efa0434df8e7c4a
Kyle <Ky@kyles-macbook-pro.local>
2016-03-07 18:38:17 -0600

    Initial Commit

---
+++ TaxCalc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TaxCalc
+//
+//  Created by Kyle on 3/7/16.
+//  Copyright © 2016 Kyle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TaxCalc/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TaxCalc
+//
+//  Created by Kyle on 3/7/16.
+//  Copyright © 2016 Kyle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
